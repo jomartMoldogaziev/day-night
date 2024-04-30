d65efcd3cd58bfbf6c16344e44cb6fc752b5992a
jomartMoldogaziev <163406907+jomartMoldogaziev@users.noreply.github.com>
2024-04-30 23:13:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const themeToggle = document.getElementById('themeToggle') // Получаем элемент переключателя
+const themeToggle = document.getElementById('themeToggle') 
 
 themeToggle.addEventListener('change', function () {
-	// Добавляем обработчик события для изменения состояния переключателя
-	Toggle() // Вызываем функцию Toggle при изменении состояния переключателя
+	
+	Toggle() 
 })
 
 const Toggle = () => {
-	// Определяем функцию Toggle
+	
 	document.body.classList.toggle('DarkBody')
-	// Если нужно, можно добавить следующие строки, чтобы переключать классы для других элементов:
-	// document.getElementById('btnToggle').classList.toggle('DarkBtn');
-	// document.getElementById('MoonAndSun').classList.toggle('Moon');
+	
 }
